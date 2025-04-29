2a736f4d33ff873d8238ef3c7cc3e0cef71fa0f7
ulriksonne02 <62388408+ulriksonne02@users.noreply.github.com>
2025-04-29 18:55:26 +0200

    Update 2_1.py

--- 2024/2/2_1.py
+++ 2024/2/2_1.py
@@ -6,28 +6,59 @@ import sys
 def safety_check(input):
     safe_reports = 0
 
+    def increasing(report):
+        adder = 1
+
+        for i in range(2,len(report)):
+            if (report[i-2] >= report[i-1]) or (report[i-1] >= report[i]):
+                #print("SAME/DECREASING")
+                adder = 0
+                break
+
+            if ((report[i-1] - report[i-2]) > 3) or ((report[i] - report[i-1]) > 3):
+                """
+                print("TOO MUCH")
+                print(report[i-1], "-", report[i-2], report[i-1] - report[i-2])
+                print(report[i], "-", report[i-1], report[i] - report[i-1])
+                """
+                adder = 0
+                break
+        
+        return adder
+
+    def decreasing(report):
+        adder = 1
+
+        for i in range(2,len(report)):
+            if (report[i-2] <= report[i-1]) or (report[i-1] <= report[i]):
+                #print("SAME/INCREASING")
+                adder = 0
+                break
+
+            if ((report[i-2] - report[i-1]) > 3) or ((report[i-1] - report[i]) > 3):
+                """
+                print("TOO MUCH")
+                print(report[i-2], "-", report[i-1], report[i-2] - report[i-1])
+                print(report[i-1], "-", report[i], report[i-1] - report[i])
+                """
+                adder = 0
+                break
+
+        return adder
+
     for i, report in enumerate(input):
         report = report.split(" ")
         report = [int(k) for k in report]
-        print(report)
+
         adder = 1
 
-        for j in range(1,len(report)-1):
-    
-            print(j, report[j])
-            if report[j] == report[j-1]:
-                adder = 0
-                break
-            elif report[j] > report[j-1]:
-                if (report[j] - report[j-1]) > 2:
-                    adder = 0
-                    break
-            
-            elif report[j] < report[j-1]:
-                if (report[j-1] - report[j]) > 2:
-                    adder = 0
-                    break
-        print(adder)
+        if report[0] < report[1]:
+            adder = increasing(report)
+        elif report[0] > report[1]:
+            adder = decreasing(report)
+        else:
+            adder = 0
+
         safe_reports += adder
     
     return safe_reports
